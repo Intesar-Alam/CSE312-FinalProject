be22ef7d77db261cfabe29169717449d71643c5a
illha <illha@host.docker.internal>
2020-04-19 22:16:40 -0400

    debugging update

--- CSE312-FinalProject/public/script2.js
+++ CSE312-FinalProject/public/script2.js
@@ -1,25 +1,25 @@
 function upvote(x) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("currVotes".concat(x.toString())).innerHTML =
-			this.responseText;
-		}
-	};
-	var votes = document.getElementById("currVotes".concat(x.toString())).innerHTML;
-	xhttp.open("GET", "upvote".concat(votes.toString()));
-	xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("currVotes".concat(x.toString())).innerHTML =
+            this.responseText;
+        }
+    };
+    var votes = document.getElementById("currVotes".concat(x.toString())).innerHTML;
+    xhttp.open("GET", "upvote".concat(votes.toString()));
+    xhttp.send();
 }
 
 function downvote(x) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("currVotes".concat(x.toString())).innerHTML =
-			this.responseText;
-		}
-	};
-	var votes = document.getElementById("currVotes".concat(x.toString())).innerHTML;
-	xhttp.open("GET", "downvote".concat(votes.toString()));
-	xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("currVotes".concat(x.toString())).innerHTML =
+            this.responseText;
+        }
+    };
+    var votes = document.getElementById("currVotes".concat(x.toString())).innerHTML;
+    xhttp.open("GET", "downvote".concat(votes.toString()));
+    xhttp.send();
 }
